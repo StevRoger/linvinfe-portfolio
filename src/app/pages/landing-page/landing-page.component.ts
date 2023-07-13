480fba6d2f987984630a7ef1febd0feac093aae0
phalla.doll <phalla.doll@okoone.com>
2023-07-13 21:54:22 +0700

    Update font

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -13,24 +13,24 @@ export class LandingPageComponent implements OnInit {
       image: '',
       logo: 'assets/logo/fb-watch.png',
       category: 'Facebook',
-      desc: 'ការបង្រៀន អនុវត្គ ចែករំលែក គន្លឹះថ្មីៗ ទាក់ទងទៅនឹង Facebook Ads-Breaks, Live, Star, Partner, ផ្សេងៗជាច្រើនទៀត យើងខ្ញុំនឹងដាក់ចូលក្នុងនេះ។',
-      link: ''
+      desc: 'ការបង្រៀន អនុវត្គ ចែករំលែក គន្លឹះថ្មីៗ ទាក់ទងទៅនឹង <span class="fw-semibold">Facebook Ads-Breaks, Live, Star, Partner</span> ផ្សេងៗជាច្រើនទៀត យើងខ្ញុំនឹងដាក់ចូលក្នុងនេះ។',
+      link: '/experience'
     },
     {
       name: 'Reels',
       image: '',
       logo: 'assets/logo/fb-reels.png',
       category: 'Facebook',
-      desc: 'ការបង្រៀន អនុវត្គ ចែករំលែក គន្លឹះថ្មីៗ ទាក់ទងទៅនឹង Facebook Reels, Ads On Reels, Ads Overlay, Bonus Performance ជាច្រើនទៀត យើងខ្ញុំនឹងដាក់ចូលក្នុងនេះ។',
-      link: ''
+      desc: `ការបង្រៀន អនុវត្គ ចែករំលែក គន្លឹះថ្មីៗ ទាក់ទងទៅនឹង <span class="fw-semibold">Facebook Reels, Ads On Reels, Ads Overlay, Bonus Performance</span>ជាច្រើនទៀត យើងខ្ញុំនឹងដាក់ចូលក្នុងនេះ។`,
+      link: '/experience'
     },
     {
       name: 'YouTube',
       image: '',
       logo: 'assets/logo/youtube-1.png',
       category: 'Google',
-      desc: 'ការបង្រៀន អនុវត្គ ចែករំលែក គន្លឹះថ្មីៗ ទាក់ទងទៅនឹង YouTube ការធ្វើ Contents បង្រៀននឹងចែករំលែកអ្វីដែលថ្មីៗ ព្រមទាំង Templates how to… ជាច្រើនទៀតដែលយើងខ្ញុំបានដាក់បញ្ចូលទៅក្នុង Website & Youtube.',
-      link: ''
+      desc: 'ការបង្រៀន អនុវត្គ ចែករំលែក គន្លឹះថ្មីៗ ទាក់ទងទៅនឹង <span class="fw-semibold">YouTube</span> ការធ្វើ <span class="fw-semibold">Contents</span> បង្រៀននឹងចែករំលែកអ្វីដែលថ្មីៗ ព្រមទាំង <span class="fw-semibold">Templates how to…</span> ជាច្រើនទៀតដែលយើងខ្ញុំបានដាក់បញ្ចូលទៅក្នុង <span class="fw-semibold">Website & Youtube.</span>',
+      link: '/experience'
     }
   ]
 
